dd01f7e92e031b3c952092cd1f6afba735f81c35
Lakshya Goel <goel0072@umn.edu>
2017-09-23 16:16:33 -0500

    Web Crawling is working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/crawl', function(req, res){
+    var pageToVisit = req.body.address;
+    console.log("Visiting page " + pageToVisit);
+    request(pageToVisit, function(error, response, body) {
+       if(error) {
+         console.log("Error: " + error);
+       }
+       console.log("Status code: " + response.statusCode);
+       if(response.statusCode === 200) {
+         var $ = cheerio.load(body);
+         res.render('web_data', { title: ($('html > body').text()) });
+       }
+    });
+});
 module.exports = router;
