fd920a1a66378b52c93e4e41c49649313d3b739b
reiven <seventhtrigger0@gmail.com>
2017-09-23 17:25:19 -0500

    summarize api integration.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
+var unirest = require('unirest');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,6 +12,20 @@ router.get('/', function(req, res, next) {
 router.post('/crawl', function(req, res){
     var pageToVisit = req.body.address;
     console.log("Visiting page " + pageToVisit);
+
+    unirest.post("https://textanalysis-text-summarization.p.mashape.com/text-summarizer")
+        .header("X-Mashape-Key", "A68IXWfzfymshQzAP0fGcYYHq9Qkp1yo1H9jsn6SuNvs3N9x7e")
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .send({"url":pageToVisit
+            ,"text":"","sentnum":8})
+        .end(function (result) {
+            // result.status 結果
+            // result.headers
+            // result.bodyが重要
+            console.log("summary",result.body);
+        });
+
     request(pageToVisit, function(error, response, body) {
        if(error) {
          console.log("Error: " + error);
