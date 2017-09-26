aa770ba3f33040a444f7d5eee7fee27f5138e7d5
reiven <seventhtrigger0@gmail.com>
2017-09-26 09:22:12 -0500

    make file sending system correctly and generate piechart from sended data.

--- functions.js
+++ functions.js
@@ -21,7 +21,16 @@ function readTestText(callback){
 }
 
 
-function parseLog(callback){
+function parseLogTest(callback){
+    /**
+     * parseLogTest()
+     *
+     * this function is load test data from /test_data/log.txt
+     * and parse it.
+     *
+     * But parseLog() is read text from client browser.
+     *
+     */
     readTestText(function(text){
         var serverLogRawText = text;
         var result = [];
@@ -48,6 +57,34 @@ function parseLog(callback){
         }
         callback(result);
     });
+}
+
+function parseLog(text, callback){
+    // console.log("hello from the function side.\n\n",text);
+    var serverLogRawText = text;
+    var result = [];
+    var i;
+    var serverLogList = serverLogRawText.split("\n");
+    var length = serverLogList.length;
+    var regex = /([0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3})\s-\s-\s\[(.*)\]\s\"(.*)\"\s([0-9]{1,3})\s([0-9]+)\s\"(.+)\"\s\"(.+)\"\s\"(.+)\"/;
+
+    for(i = 0; i < length; i++){
+        var lineItem = serverLogList[i].match(regex);
+        if(lineItem){
+            var appendObject = {
+                "IP": lineItem[1],// TODO: if IP is not given like "" or "-", then data never match now...
+                "Time": lineItem[2],//TODO: need to wrap datetime?
+                "HTTP": lineItem[3],
+                "Status": lineItem[4],
+                "Size": parseInt(lineItem[5]),
+                "Referer": lineItem[6],
+                "UA": lineItem[7]
+            };
+            // console.log("obj", appendObject);
+            result.push(appendObject);
+        }
+    }
+    callback(result);
 }
 
 
---
+++ routes/reiven.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+// parse User Agent
+var uaParser = require('ua-parser-js');
+// add multer lib to support file uploads
+var multer  = require('multer')
+var upload = multer({ storage: multer.memoryStorage() });
+
+router.get('/', function (req, res, next) {
+    res.render('indexReiven', {
+        title: 'Reiven Test'
+    });
+});
+
+
+router.post('/show-graph', upload.single("thefile") ,function(req, res, next){
+    // TODO: make json from test data and send it.
+    /**
+     * 4. chart it in embed script tags first to test.
+     *      - bar chart
+     *      - line graph
+     */
+
+    var functions = require("../functions");
+
+    if (!req.file) {
+        res.status(500).send('error: no file');
+    }
+
+    // console.log(req.file.buffer.toString('UTF-8'));
+
+    functions.parseServerLog(req.file.buffer.toString('UTF-8') ,function(data){
+        // console.log("inside functions.parseServerLog", data);
+        var UA = {};
+        var sendData = [];
+        var i;
+
+        for(i = 0; i < data.length; i++){
+            // console.log(data[i]["UA"]);
+            var uaData = uaParser(data[i]["UA"]);
+            var os = uaData["os"]["name"];
+            var appendData = uaData["browser"]["name"] + " on ";
+            if(os == "Windows"){
+                appendData += os + uaData["os"]["version"];
+            }else if(os == "Mac OS" || os == "Android" || os == "iOS"){
+                appendData += os;
+            }else{
+                appendData = "others";
+            }
+            if(Object.keys(UA).indexOf(appendData) >= 0){
+                UA[appendData] += 1;
+            }else{
+                UA[appendData] = 1;
+            }
+        }
+        // console.log(UA);
+        for(i = 0; i < Object.keys(UA).length; i++){
+            sendData.push({
+                "key": Object.keys(UA)[i], "value": UA[Object.keys(UA)[i]]
+            });
+        }
+        res.render('test_nvd3', {title: 'NVD3 test', UA: sendData});
+    });// end functions.parseServerLog;
+});
+
+module.exports = router;
