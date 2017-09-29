49dcd60cbf72b042c12a2fe3d348602c80a2bdab
Nishka Monteiro <nishkamonteiro@gmail.com>
2017-09-29 00:58:02 -0500

    Bar Graph

--- routes/reiven.js
+++ routes/reiven.js
@@ -38,7 +38,7 @@ router.post('/show-graph', upload.single("thefile") ,function(req, res, next){
         var fileTypeData = [];
         var barChartData1 = [];
         var barChartData2 = [];
-        var aMap = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        var aMap = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
         var i;
         var type
@@ -82,7 +82,7 @@ router.post('/show-graph', upload.single("thefile") ,function(req, res, next){
             // data parsing for lineChart
 
             // data parsing for barChart
-            var hr =data[i]["Time"].hours();
+            var hr =data[i]["Time"].format("H");
             var sz =data[i]["Size"];
             aMap[hr]= aMap[hr]+sz;
 
@@ -99,7 +99,7 @@ router.post('/show-graph', upload.single("thefile") ,function(req, res, next){
         }
         for(i = 12; i <=23; i++){
             barChartData2.push({
-                  "key": i, "value": aMap[i]
+                  "key": i-12, "value": aMap[i]
               });
         }
 
